733ed36d56bf3a2265ba11b91a5e52997a9be15c
Bruna <garciabrunap@gmail.com>
2018-01-31 20:50:30 -0800

    part3 - workInProgress

--- index.js
+++ index.js
@@ -11,21 +11,48 @@ const spoilerTimer = Number(readlineSync.question('Enter wait time to your spoil
 const spoilerContent = `Darth Vader is Anakin Skywalker, Luke's dad`;
 
 
-function spoilerFunc(movieName){
-  if (!movieName){
+function spoilerFunc(movieName) {
+  if (!movieName) {
     console.log(`That movie does not exist! No spoilers!`)
-  }else{
-    console.log(`The ${movieName} spoiler is going to appear in ${spoilerTime} seconds...`)
-    //display spoiler to user
-    setTimeout(() => {console.log(spoilerContent)}, spoilerTimer*1000);
+  } else {
+    console.log(`The ${movieName} spoiler is going to appear in ${spoilerTimer} seconds...`)
+    //process through API
+
+    //declare url to be displayed
+    var url = `https://www.google.ca/search?q=${movieName}`
+
+    request(url, function (error, response, body) {
+      if (!error) {
+        //creating array to keep heading after parse
+        var headingsArr = []
+        //getting heading with cheerio
+        var $ = cheerio.load(body),
+          //Get heading and passing in a variable --> heading are children of #ires container.
+          searchHeadings = $("#ires").children();
+        searchHeadings.each(function (index) {
+          let filtered = Array($("#ires").children().find(".r").text());
+          if (filtered !== "") {
+            // headingsArr.push(filtered);
+
+            console.log(filtered[0]);
+            //forEach loop to print the array to console.
+            // headingsArr.forEach(function (h1) {
+            //   console.log(h1);
+            // })
+          } else {
+            console.log(err)
+          }
+          //display spoiler to user
+          setTimeout(() => {
+            console.log(spoilerContent)
+          }, spoilerTimer * 1000);
+        })
+      }
+    })
   }
-
-  //process through API
-
-  //get spoiler
-
 }
 //Use movieInput as parameter for function.
 spoilerFunc(movieInput);
 
 // search line for searching on google https://www.google.ca/search?q=movie
+//.eq(index) line 33
