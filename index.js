d731b2c99f2fa15bd990692555ce212784fc5aad
Bruna <garciabrunap@gmail.com>
2018-04-27 13:33:16 -0700

    final adjusts

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const readlineSync = require('readline-sync');
 const cheerio = require('cheerio');
 
 //get movie name from user:
-const movieInput = readlineSync.question('Enter a movie name? ');
+const movieInput = readlineSync.question('\nOh hi hello! \nPlease enter a movie name? ');
 //Spoiler time wait
-const spoilerTimer = Number(readlineSync.question('Enter wait time in seconds to get your spoiler? '));
+const spoilerTimer = Number(readlineSync.questionInt('\nEnter wait time in seconds to get your spoiler? '));
 
 if (movieInput === "" || spoilerTimer === 0) {
   //error in case movie and/or spoiler time are null -- Diving deeper
-  console.log(`The movie and/or the time to spoil is null! No spoilers for that!`);
+  console.log(`\nThe movie and/or the time to spoil is null! No spoilers for that!`);
   return;
 } else if (spoilerTimer < 0) {
-  console.log(`The spoiler time is invalid, try again! `);
+  console.log(`\nThe spoiler time is invalid, try again! `);
   return;
 }
 
@@ -29,7 +29,7 @@ function scrapper() {
 
       var $ = cheerio.load(body)
 
-      console.log(`=> While you wait, read some news about ${movieInput}:`)
+      console.log(`\n=> While you wait, read some news about ${movieInput}:`)
 
       $('h3').each(function (i, elem) {
         headingsArr[i] = $(this).text();
@@ -47,15 +47,17 @@ request(urlData, ((err, response, data) => {
   moviesData = JSON.parse(data);
 
   if (moviesData.results.length > 0) {
-    console.log(`***** The ${movieInput} spoiler is going to appear in ${spoilerTimer} seconds...*****`)
+    console.log(`\n***** The ${movieInput} spoiler is going to appear in ${spoilerTimer} seconds...*****`)
     scrapper();
     spoilerTitle = String(moviesData.results[0].title);
     spoilerContent = String(moviesData.results[0].overview);
 
     setTimeout(() => {
-      console.log(`*** The ${spoilerTitle} spoiler is:`);
-      console.log(`${spoilerContent}`);
+      console.log(`\n*** The ${spoilerTitle} spoiler is:`);
+      console.log(`\n${spoilerContent}`);
+      console.log(`\n you got it nerd!`)
     }, spoilerTimer * 1000);
+
 
   } else {
     console.log(`Movie name not found in our database!!! Try again!`);
